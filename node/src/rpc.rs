265b58774679eae450de5600b829d713edda4376
Shunsuke Watanabe <ww.shunsuke@gmail.com>
2020-11-18 14:21:40 +0900

    fix

--- node/src/rpc.rs
+++ node/src/rpc.rs
@@ -36,6 +36,7 @@ pub fn create_full<C, P>(
 	C::Api: substrate_frame_rpc_system::AccountNonceApi<Block, AccountId, Index>,
 	C::Api: pallet_transaction_payment_rpc::TransactionPaymentRuntimeApi<Block, Balance>,
 	C::Api: BlockBuilder<Block>,
+	C::Api: pallet_contracts_rpc::ContractsRuntimeApi<Block, AccountId, Balance, BlockNumber>,
 	P: TransactionPool + 'static,
 {
 	use substrate_frame_rpc_system::{FullSystem, SystemApi};
